7d0097081cb9567f62437cb4e94fd7ce2f56af5b
EtreSerBe <augonzalez@uartesdigitales.edu.mx>
2017-07-23 18:34:40 -0500

    first commit

---
+++ Assets/Scripts/MultiGravityController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiGravityController : MonoBehaviour {
+
+    public Vector3 m_vActualGravityDirection;
+    public float m_fButtonDelay = 0.2f; //in seconds
+    public float m_fCurrentElapsedTime = 0.0f;
+
+    public HashSet<ParalyzableObject> m_hsParalyzableObjects = new HashSet<ParalyzableObject>();
+
+
+    void UpdateParalyzableObjects()
+    {
+        foreach ( ParalyzableObject PO in m_hsParalyzableObjects )
+        {
+            PO.DecreaseParalysis(); 
+            //Also, if it is no longer paralyzed, then update the Walkable part of the object.
+        }
+    }
+
+    void RotateObject()
+    {
+        float fAngle = Vector3.Angle(transform.up, Physics.gravity);
+        transform.right = Vector3.Cross(-Physics.gravity.normalized, transform.forward);
+        transform.up = -Physics.gravity.normalized;
+        //gameObject.transform.Rotate(transform.forward, fAngle);
+    }
+
+	// Use this for initialization
+	void Start () {
+        m_vActualGravityDirection = Physics.gravity;
+
+        //CAREFUL: This is not ready to react to NEWLY ADDED PARALYZABLE OBJECTS, so be careful.
+        m_hsParalyzableObjects.UnionWith( FindObjectsOfType<ParalyzableObject>()); //Add all the found Paralyzable objects.
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+        float fHorizontalInput = Input.GetAxis("Horizontal");
+        float fVerticalInput = Input.GetAxis("Vertical");
+        m_fCurrentElapsedTime += Time.deltaTime;
+        if (Input.GetButtonDown("ChangeGravity") && m_fCurrentElapsedTime > m_fButtonDelay)
+        {
+            if (Mathf.Abs(fHorizontalInput) > 0.0f || Mathf.Abs(fVerticalInput) > 0.0f)
+            {
+                RotateObject();
+                m_vActualGravityDirection = (transform.up * fVerticalInput + transform.right * fHorizontalInput).normalized * 9.81f;
+                Physics.gravity = m_vActualGravityDirection;
+                Debug.Log("Changing gravity to: " + m_vActualGravityDirection.ToString());
+                m_fCurrentElapsedTime = 0.0f; //reset it so the player has to wait a while before re-changing the gravity.
+            }
+        }
+        
+	}
+}
---
+++ Assets/Scripts/ParalyzableObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalyzableObject : MonoBehaviour {
+
+    public bool m_bIsParalyzed = false;
+    public int m_iMaxParalysis = 1; //The max number of "RemainingParalysis" this object can stack.
+    public int m_iRemainingParalysis = 0; //The number of times it will remain static before responding to gravity again.
+
+
+    //public Collider m_pCollider
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Paralysis"))
+        {
+            m_iRemainingParalysis = m_iRemainingParalysis + 1 > m_iMaxParalysis ? m_iMaxParalysis : m_iRemainingParalysis + 1; //limit it to the max
+        }
+    }
+
+    //This function has to be called from the Multi gravity controller, so it decreases all the paralyzable objects' paralysis by 1.
+    public void DecreaseParalysis()
+    {
+        m_iRemainingParalysis = m_iRemainingParalysis < 0 ? m_iRemainingParalysis -1 :0; //Limit it from below to 0.
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
