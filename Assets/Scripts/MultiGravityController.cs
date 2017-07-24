b8ff07eaa7bd4c78bb9fee15f2fd42ef8bad22a9
EtreSerBe <augonzalez@uartesdigitales.edu.mx>
2017-07-23 19:11:18 -0500

    working on the controller, it still does what it wants.

--- Assets/Scripts/MultiGravityController.cs
+++ Assets/Scripts/MultiGravityController.cs
@@ -7,8 +7,18 @@ public class MultiGravityController : MonoBehaviour {
     public Vector3 m_vActualGravityDirection;
     public float m_fButtonDelay = 0.2f; //in seconds
     public float m_fCurrentElapsedTime = 0.0f;
+    public float m_fHorizontalSensitivity = 20.0f;
+    public float m_fVerticalSensitivity = 10.0f;
+    public float m_fMovementAccelerationFactor = 1.0f;
+
+    public GameObject m_pParalysisGunRef;
+    public GameObject m_pParalysisBulletRef;
 
     public HashSet<ParalyzableObject> m_hsParalyzableObjects = new HashSet<ParalyzableObject>();
+    protected HashSet<GameObject> m_hsParalysisBullets = new HashSet<GameObject>();
+
+    private int m_iBulletActualID = 0;
+    protected Rigidbody m_pRigidbodyRef;
 
 
     void UpdateParalyzableObjects()
@@ -28,31 +38,78 @@ public class MultiGravityController : MonoBehaviour {
         //gameObject.transform.Rotate(transform.forward, fAngle);
     }
 
-	// Use this for initialization
-	void Start () {
+    public void EraseBullet(GameObject in_pBulletToErase)
+    {
+        if (m_hsParalysisBullets.Contains(in_pBulletToErase))
+        {
+            m_hsParalysisBullets.Remove(in_pBulletToErase);
+        }
+        else
+        {
+            Debug.LogWarning("Somehow, a bullet tried to be erased twice or more. It had the ID: " + in_pBulletToErase.name);
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
         m_vActualGravityDirection = Physics.gravity;
 
         //CAREFUL: This is not ready to react to NEWLY ADDED PARALYZABLE OBJECTS, so be careful.
         m_hsParalyzableObjects.UnionWith( FindObjectsOfType<ParalyzableObject>()); //Add all the found Paralyzable objects.
+        m_pRigidbodyRef = GetComponentInChildren<Rigidbody>();
 
-	}
+        if (m_pParalysisBulletRef == null)
+        {
+            Debug.LogError("Error, no paralysisbullet reference provided.");
+        }
+    }
 
     // Update is called once per frame
     void Update() {
         float fHorizontalInput = Input.GetAxis("Horizontal");
         float fVerticalInput = Input.GetAxis("Vertical");
         m_fCurrentElapsedTime += Time.deltaTime;
-        if (Input.GetButtonDown("ChangeGravity") && m_fCurrentElapsedTime > m_fButtonDelay)
+        if ((Mathf.Abs(fHorizontalInput) > 0.0f || Mathf.Abs(fVerticalInput) > 0.0f))
         {
-            if (Mathf.Abs(fHorizontalInput) > 0.0f || Mathf.Abs(fVerticalInput) > 0.0f)
+            if (Input.GetButtonDown("ChangeGravity"))
             {
-                RotateObject();
-                m_vActualGravityDirection = (transform.up * fVerticalInput + transform.right * fHorizontalInput).normalized * 9.81f;
-                Physics.gravity = m_vActualGravityDirection;
-                Debug.Log("Changing gravity to: " + m_vActualGravityDirection.ToString());
-                m_fCurrentElapsedTime = 0.0f; //reset it so the player has to wait a while before re-changing the gravity.
+                if (m_fCurrentElapsedTime > m_fButtonDelay)
+                {
+                    m_vActualGravityDirection = (transform.up * fVerticalInput + transform.right * fHorizontalInput).normalized * 9.81f;
+                    Physics.gravity = m_vActualGravityDirection;
+                    Debug.Log("Changing gravity to: " + m_vActualGravityDirection.ToString());
+                    m_fCurrentElapsedTime = 0.0f; //reset it so the player has to wait a while before re-changing the gravity.
+                }
+            }
+            else
+            {
+                //Then, move normally.
+                //NOTE: this should take a FORWARD ignoring the facing of the camera (stripping it from the Y axis), 
+                Vector3 pMovementVector = fHorizontalInput * transform.right + fVerticalInput * transform.forward;
+                //pMovementVector.y = 0.0f;
+                m_pRigidbodyRef.AddForce(pMovementVector * m_fMovementAccelerationFactor, ForceMode.Force);
             }
         }
-        
-	}
+
+        /********************** CAMERA MOVEMENT ****************/
+        float fRightHorizontal = Input.GetAxis("RightHorizontal");
+        float fRightVertical = Input.GetAxis("RightVertical");
+
+        if (Mathf.Abs(fRightHorizontal) > 0.0f || Mathf.Abs(fRightVertical) > 0.0f)
+        {
+            Quaternion tOtherQuat = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(fRightHorizontal * m_fHorizontalSensitivity, fRightVertical * m_fVerticalSensitivity, 0.0f));
+            //How can one get the Acceleration?
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, tOtherQuat, 180.0f); //180????
+        }
+
+        if (Input.GetButtonDown("Shoot"))
+        {
+            //Create a new bullet with the position and rotation of the Gun which fires it.
+            //NOTE: The gunRef must facing similar direction as the player camera (but adjusted like an FPS weapon.).
+            GameObject pNewBullet = Instantiate<GameObject>(m_pParalysisBulletRef, m_pParalysisGunRef.transform.position, m_pParalysisGunRef.transform.rotation);
+            pNewBullet.name = "Bullet " + m_iBulletActualID.ToString();
+            m_hsParalysisBullets.Add(pNewBullet);
+        }
+    }
 }
---
+++ Assets/Scripts/ParalysisBullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalysisBullet : MonoBehaviour {
+
+    public float m_fInitialImpulse = 1.0f;
+    public float m_fMaxSpeed = 20.0f;
+    public float m_fMinSpeed = 1.0f;
+    public float m_fTimeToLive = 5.0f;
+    //public float m_f
+    private Rigidbody m_pRigidbodyRef;
+    private MultiGravityController m_pMultiGravityControllerRef;
+
+	// Use this for initialization
+	void Start () {
+        m_pRigidbodyRef = GetComponent<Rigidbody>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CancelInvoke();
+        //Maybe I should also disappear it, so it doesn't look weird.
+        m_pMultiGravityControllerRef.EraseBullet(gameObject); //Erase this game object from the Bullet manager.
+    }
+
+    void EndTimeToLive()
+    {
+        m_pMultiGravityControllerRef.EraseBullet(gameObject); //Erase this game object from the Bullet manager.
+    }
+
+    public void Shoot( MultiGravityController in_pMGCref )
+    {
+        m_pMultiGravityControllerRef = in_pMGCref;
+        m_pRigidbodyRef.AddForce(transform.forward*m_fInitialImpulse, ForceMode.Impulse);
+        Invoke("EndTimeToLive", m_fTimeToLive);//Invoked only if it does not collide with something, so it doesn't live forever.
+    }
+
+	// Update is called once per frame
+	void Update () {
+		//SHOULD THIS HAVE AN UPDATE? I don't think so.
+        //Maybe I could add different funny behaviors, like bullet which stop in motion for a while, and can be used for crazy designs. :)
+	}
+}
